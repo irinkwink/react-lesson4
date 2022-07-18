9ba05b4ce702affc09213834c7d65a763dab6007
irinkwink <irina-ns@yandex.ru>
2022-07-18 12:31:57 +0300

    done lesson

--- src/components/FunctionalComponent/FunctionalComponent.jsx
+++ src/components/FunctionalComponent/FunctionalComponent.jsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import {useState} from 'react';
 import style from './FunctionalComponent.module.css';
+import PropTypes from 'prop-types';
 
-export const FunctionalComponent = (props) => (
-  <div className={style.game}>
-    <p className={style.result}>5</p>
-    <form className={style.form}>
-      <label className={style.label} htmlFor='user_number'>
-        Угадай число
-      </label>
-      <input className={style.input} type='number' id='user_number' />
-      <button className={style.btn}>Угадать</button>
-    </form>
-  </div>
-);
+export const FunctionalComponent = ({min, max}) => {
+  const [userNumber, setUserNumber] = useState('');
+  const [result, setResult] = useState('Результат');
+  const [count, setCount] = useState(0);
+  const [randomNumber] = useState(
+    Math.floor(Math.random() * (max - min + 1)) + min
+  );
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setCount(prevCount => prevCount + 1);
+
+    setResult(() => {
+      if (!userNumber || userNumber < min || userNumber > max) {
+        return `Введите число от ${min} до ${max}`;
+      }
+
+      if (userNumber > randomNumber) {
+        return `${userNumber} больше загаданного числа`;
+      }
+
+      if (userNumber < randomNumber) {
+        return `${userNumber} меньше загаданного числа`;
+      }
+
+      return `Угадали, загаданное число ${userNumber}`;
+    });
+  };
+
+  const handleChange = e => {
+    setUserNumber(e.target.value);
+  };
+
+  return (
+    <div className={style.game}>
+      <p className={style.result}>{result}</p>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <label className={style.label} htmlFor='user_number'>
+          Попыток: {count}
+        </label>
+        <input
+          className={style.input}
+          type='number'
+          id='user_number'
+          value ={userNumber}
+          onChange ={handleChange}
+        />
+        <button className={style.btn}>Угадать</button>
+      </form>
+    </div>
+  );
+};
+
+FunctionalComponent.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
